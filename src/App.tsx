77332814d770f02065ea90bc96a13763e040e01e
zxcv3508 <zxcv3508@navre.com>
2023-08-21 16:24:17 +0900

    add: recoil, directorys

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Main from "./pages/main/index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 function App() {
+  const a = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>test</h1>
-      </header>
-    </div>
+    <>
+      <RecoilRoot>{a ? <PrivateRouter /> : <PublicRouter />}</RecoilRoot>
+    </>
+  );
+}
+
+function PrivateRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<PrivateLayout />}> */}
+        <Route path="/" element={<Main />} />
+        {/* <Route path="/*" element={<Error />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function PublicRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        {/* <Route index element={<Login />} /> */}
+        {/* <Route path="/login/*" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/expire" element={<Expire />} />
+          <Route path="/500" element={<ServerError />} />
+          <Route path="/*" element={<Error />} />
+          <Route path="/item/edit" element={<ItemEdit />} />
+          <Route path="/item/add" element={<ItemAdd />} /> */}
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
