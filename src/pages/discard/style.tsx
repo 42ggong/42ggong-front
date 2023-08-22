627c64f07ea17be98a8392684f72d2197a84849c
zxcv3508 <zxcv3508@navre.com>
2023-08-22 18:03:48 +0900

    feat: in mylist page, ListBoxForm become components

--- src/pages/discard/style.tsx
+++ src/pages/discard/style.tsx
@@ -1,3 +1,51 @@
 import styled from "@emotion/styled";
 
-export const Div = styled.div``;
+// inform
+export const InformContainer = styled.div``;
+
+// list box
+export const ListBoxContainer = styled.div``;
+
+export const ListBoxBorder = styled.div`
+  border: 1px solid;
+`;
+
+export const ListBoxButtonContainer = styled.div``;
+
+export const ListBoxForm = styled.form`
+  width: 85%;
+  padding: 10px 25px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin: 15px 0;
+`;
+
+// list item
+export const ListItemBorder = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid;
+`;
+
+export const ListItemCheckBox = styled.input`
+  // position: relative;
+  // top: 5.5px;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  border: 1px solid #999;
+  appearance: none;
+  cursor: pointer;
+  transition: background 0.2s;
+  align-items: center;
+  :checked {
+    background: green;
+    border: none;
+  }
+`;
+
+export const ListItemColumn = styled.div`
+  width: 100px;
+  border: 1px solid;
+`;
---
+++ src/pages/myList/componets/ListBoxForm/index.tsx
@@ -0,0 +1,158 @@
+import React, { useState, useEffect } from "react";
+import * as S from "../../style";
+import Modal from "../../../../componets/Modal/index";
+
+interface Iprops {
+  dummyList: { uid: string; expday: string; info: string }[];
+}
+
+const today = new Date().toISOString().slice(5, 10);
+
+const ListBoxForm = ({ dummyList }: Iprops) => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalText, setModalText] = useState("");
+  const [allChecked, setAllChecked] = useState(false);
+  const [checkedArr, setCheckedArr] = useState(
+    Array.from({ length: dummyList.length }, () => false)
+  );
+
+  useEffect(() => {
+    if (checkedArr.length < 1) setAllChecked(false);
+    else if (checkedArr.indexOf(false) === -1) setAllChecked(true);
+    else setAllChecked(false);
+  }, [checkedArr]);
+
+  const generateButton = (listElement: any) => {
+    if (listElement.expday > today)
+      return (
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setModalText(
+              `${listElement.uid} ${listElement.info} 를 처리합니다.
+				반드시 냉장고에서
+				꺼낸 후에 완료해주세요!`
+            );
+            setShowModal(true);
+          }}
+          tabIndex={-1}
+        >
+          찾기
+        </button>
+      );
+    return (
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setModalText(
+            `${listElement.uid} ${listElement.info} 를 처리합니다.
+			  반드시 냉장고에서
+			  꺼낸 후에 완료해주세요!`
+          );
+          setShowModal(true);
+        }}
+        tabIndex={-1}
+      >
+        폐기
+      </button>
+    );
+  };
+  const countChecked = () => {
+    return checkedArr.filter((element) => element === true).length;
+  };
+
+  const generateText = () => {
+    return;
+  };
+
+  const onSubmit = () => {
+    dummyList = dummyList.filter(
+      (element: any, index: any) => checkedArr[index] === false
+    );
+    setCheckedArr(Array.from({ length: dummyList.length }, () => false));
+    setModalText("");
+    setShowModal(false);
+  };
+
+  const onCloseModal = () => {
+    setModalText("");
+    setShowModal(false);
+  };
+
+  const onMultiDiscard = (e: any) => {
+    e.preventDefault();
+    const checkedLength = countChecked();
+    setModalText(`총 ${checkedLength}개의 보관품을
+	  처리를 합니다.
+	  반드시 냉장고에서
+	  꺼낸 후에 완료해주세요!
+	  `);
+    setShowModal(true);
+  };
+  return (
+    <>
+      <S.ListBoxForm>
+        <S.ListItemBorder style={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}>
+          <S.ListItemCheckBox
+            type="checkbox"
+            checked={allChecked}
+            tabIndex={-1}
+            onChange={() => {
+              if (checkedArr.indexOf(false) === -1) {
+                setAllChecked(false);
+                setCheckedArr(
+                  Array.from({ length: dummyList.length }, () => false)
+                );
+              } else {
+                setAllChecked(true);
+                setCheckedArr(
+                  Array.from({ length: dummyList.length }, () => true)
+                );
+              }
+            }}
+          />
+          <S.ListItemColumn>식별자</S.ListItemColumn>
+          <S.ListItemColumn>유효기간</S.ListItemColumn>
+          <S.ListItemColumn>설명</S.ListItemColumn>
+          <S.ListItemColumn> </S.ListItemColumn>
+        </S.ListItemBorder>
+        {dummyList.map((element: any, index: number) => {
+          return (
+            <S.ListItemBorder key={element.uid}>
+              <S.ListItemCheckBox
+                type="checkbox"
+                checked={checkedArr[index]}
+                tabIndex={-1}
+                onChange={() => {
+                  const tmpArr = [...checkedArr];
+                  tmpArr[index] = !checkedArr[index];
+                  setCheckedArr(tmpArr);
+                }}
+              />
+              <S.ListItemColumn>{element.uid}</S.ListItemColumn>
+              <S.ListItemColumn>{element.expday}</S.ListItemColumn>
+              <S.ListItemColumn>{element.info}</S.ListItemColumn>
+              <S.ListItemColumn>{generateButton(element)}</S.ListItemColumn>
+            </S.ListItemBorder>
+          );
+        })}
+        <button
+          onClick={onMultiDiscard}
+          disabled={countChecked() < 1}
+          tabIndex={-1}
+        >
+          선택 처리
+        </button>
+      </S.ListBoxForm>
+      {showModal && (
+        <Modal
+          handleClose={onCloseModal}
+          handleSubmit={onSubmit}
+          text={modalText}
+        />
+      )}
+    </>
+  );
+};
+
+export default ListBoxForm;
